8e75c55df0930a0e6813a1a9b0d71fea8f2c8236
Venkanna-1009 <venkannachowdary2000@gmail.com>
2025-07-24 15:49:50 +0530

    Evaluation is added

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from src.datascience.pipeline.Data_Ingestion_Pipeline import DataIngestionTraini
 from src.datascience.pipeline.Data_Validation_Pipeline import DataValidationTrainingPipeline
 from src.datascience.pipeline.Data_Transformation_Pipeline import DataTransformationTrainingPipeline
 from src.datascience.pipeline.Model_trainer_Pipeline import ModelTrainerTrainingPipeline
-
+from src.datascience.pipeline.model_evaluation_Pipeline import ModelEvaluationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -53,3 +53,14 @@ except Exception as e:
     logger.exception(e)
     raise e
 
+
+STAGE_NAME = "model evaluation stage"
+
+try:
+    logger.info(f">>>>>>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<<<<")
+    model_evaluation = ModelEvaluationTrainingPipeline()
+    model_evaluation.initiate_model_evaluation()
+    logger.info(f">>>>>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<<<<<<<<\n\nx================x")
+except Exception as e:
+    logger.exception(e)
+    raise e 
--- src/datascience/config/configuration.py
+++ src/datascience/config/configuration.py
@@ -1,4 +1,4 @@
-from src.datascience.entity.config_entity import (DataIngestionConfig,DataValidationConfig,DataTransformationConfig,ModelTrainerConfig)
+from src.datascience.entity.config_entity import (DataIngestionConfig,DataValidationConfig,DataTransformationConfig,ModelTrainerConfig,ModelEvaluationConfig)
 from src.datascience.constants import *
 from src.datascience.utils.common import read_yaml, create_directories
 
@@ -70,3 +70,23 @@ class ConfigurationManager:
             target_column = schema.name
         )
         return model_trainer_config
+    
+
+    def get_model_evaluation_config(self) -> ModelEvaluationConfig:
+        config = self.config.model_evaluation
+        params = self.params.ElasticNet
+        schema = self.schema.TARGET_COLUMN
+
+        create_directories([config.root_dir])
+
+        model_evaluation_config = ModelEvaluationConfig(
+            root_dir = config.root_dir,
+            test_data_path = config.test_data_path,
+            model_path = config.model_path,
+            all_params = params,
+            metric_file_name = config.metric_file_name,
+            target_column = schema.name,
+            mlflow_uri = "https://dagshub.com/venkannachowdary2000/Datascience-Project-AWS.mlflow"
+
+        )
+        return model_evaluation_config
